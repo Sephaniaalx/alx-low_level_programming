716aacea9b9f874f2f500f4ee0543d09439ae53b
Sephaniaalx <koutanyi@gmail.com>
2023-06-20 17:01:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - prints the word _putchar
+ * @c: refers to the character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	char c = _putchar
+
+	putchar(c)
+	return (1);
+}
