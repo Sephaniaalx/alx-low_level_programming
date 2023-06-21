5729a83e30b00ec942a09d6951a8fe234094afef
Sephaniaalx <koutanyi@gmail.com>
2023-06-20 18:53:03 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _islower - function prints the lower character
+ *
+ * Return 1 if c but 0 if not
+ *
+ */
+
+int _islower(int c);
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+if (i = 'c')
+{
+return 1;
+}
+else
+{
+return 0;
+}
+}
