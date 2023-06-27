453833ef322d67d82c7c43b46b0bace6f49d9756
Sephaniaalx <koutanyi@gmail.com>
2023-06-26 18:54:10 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _strlen - representing the string length
+ * @s: address of pointer
+ *
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		u++;
+	}
+	return (0);
+}
