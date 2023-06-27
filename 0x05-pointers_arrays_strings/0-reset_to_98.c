16d87109ba159fbf30f6badebd56c6769c8f98e0
Sephaniaalx <koutanyi@gmail.com>
2023-06-26 18:30:15 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - function that takes a pointer to an int
+ * @*n: pointer to a variable n
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * All header files present
+ *
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
