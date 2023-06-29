a2483126351330236acf39c14a719516cbabb1b1
Sephaniaalx <koutanyi@gmail.com>
2023-06-29 14:54:18 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int j;
-	int i;
+	int j = 0;
+	int i = 0;
 	
 	while(dest[i] != '\0')
 	{
 		i++;
 	}
+
 	while(src[j] != '\0')
 	{
+		dest[i] = src[j];
 		j++;
-		i++
+		i++;
 	}
 	dest[i] = '\0';
 
-	return (dest);
+	return(dest);
 }
