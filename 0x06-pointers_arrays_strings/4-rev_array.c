0968b119e96252677ab87e76621b5d4f4fba6a2f
Sephaniaalx <koutanyi@gmail.com>
2023-06-29 22:30:45 -0700

    atonce

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * reverse_array - function that reverses the array
+ * @a: array to be reversed
+ * @n: number of elements
+ *
+ * return: zero
+ */
+
+void reverse_array
+{
+}
