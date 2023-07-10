5c2b86f225134b357cff80649981444a8007524e
Sephaniaalx <koutanyi@gmail.com>
2023-07-10 14:09:54 -0700

    hello

---
+++ 1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: the count
+ * @argv: the value
+ * Return: 0 upon success
+ */
+
+int main(int argc, char* argv[] __attribute((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
