a06355038c19ce227986613f85a637a15d7654d3
Sephaniaalx <koutanyi@gmail.com>
2023-07-10 15:00:45 -0700

    changes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,11 @@
 /**
  * main - thats the main function
  * @argc: this represent the count
- * @argv[]: this represents the array
  * @argv: this represents the string
  * Return: returns 0 on success
  */
 
-int main(int argc __attribute((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
