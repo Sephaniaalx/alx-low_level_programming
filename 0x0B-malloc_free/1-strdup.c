e157e47bb45bf30d0e8c5c7287393ab48e9a7e96
Sephaniaalx <koutanyi@gmail.com>
2023-07-11 18:44:31 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - the main function
+ * @str: the string
+ *
+ * Return: Null
+ */
+char *_strdup(char *str)
+{
+	char *point;
+	int length;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (length = 0; str[length]; length++)
+	point = malloc(sizeof(char) * length);
+	if (!point)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < length; i++)
+	{
+		point[i] = str[i];
+	}
+	point[i] = '\0';
+	return (point);
+}
